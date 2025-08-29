a736df83ac8ac42293ef30dbf606f9f9365d7181
0xySan <ethtaquet@gmail.com>
2025-08-29 16:52:10 +0200

    made dup smaller and pop to not segfault

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,42 +1,15 @@
-default rel
-
 section .text
     global ft_strdup
     extern malloc
-
-ft_strlen:
-    XOR rax, rax
-
-.lenloop:
-    CMP BYTE [rdi + rax], 0
-    JZ .lendone
-    INC rax
-    JMP .lenloop
-
-.lendone:
-    ret
-
-ft_strcpy:
-    XOR rax, rax
-
-.cpyloop:
-    CMP BYTE [rsi + rax], 0
-    MOV dl, BYTE [rsi + rax]
-    MOV BYTE [rdi + rax], dl
-    JZ .cpydone
-    INC rax
-    JMP .cpyloop
-
-.cpydone:
-    MOV rax, rdi
-    ret
+    extern ft_strcpy
+    extern ft_strlen
 
 ft_strdup:
     PUSH rdi 
     CALL ft_strlen
     INC rax
     MOV rdi, rax
-    CALL malloc wrt ..plt
+    CALL malloc
     CMP rax, 0
     JZ .malloc_fail
     MOV rdi, rax
@@ -46,4 +19,5 @@ ft_strdup:
 
 .malloc_fail:
     XOR rax, rax
+    POP rsi
     ret
